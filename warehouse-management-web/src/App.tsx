b9f403e74150b33028d24931053e326adf9e3bab
小浩 <sdwenhappy@163.com>
2022-05-26 22:57:28 +0800

    fix:

--- warehouse-management-web/src/App.tsx
+++ warehouse-management-web/src/App.tsx
@@ -9,30 +9,33 @@ export const App = () => {
     return (
         <div className={styles.app}>
             <div className={styles.header}>
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    onSelect={({ key }) => {
-                        navigator(key);
-                    }}
-                    defaultSelectedKeys={["2"]}
-                    items={
-                        [
-                            {
-                                key: "product",
-                                label: "仓库产品"
-                            },
-                            {
-                                key: "action/入库",
-                                label: "入库记录"
-                            },
-                            {
-                                key: "action/出库",
-                                label: "出库记录"
-                            }
-                        ]
-                    }
-                />
+                <div className={styles.left}>仓库在线管理系统</div>
+                <div className={styles.menu}>
+                    <Menu title="仓库在线管理系统"
+                          theme="dark"
+                          mode="horizontal"
+                          onSelect={({key}) => {
+                              navigator(key);
+                          }}
+                          defaultSelectedKeys={["2"]}
+                          items={
+                              [
+                                  {
+                                      key: "product",
+                                      label: "仓库产品"
+                                  },
+                                  {
+                                      key: "action/入库",
+                                      label: "入库记录"
+                                  },
+                                  {
+                                      key: "action/出库",
+                                      label: "出库记录"
+                                  }
+                              ]
+                          }
+                    />
+                </div>
             </div>
             <div className={styles.content}>
                 {outlet}
